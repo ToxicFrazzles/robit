df54cc25248f6f9436b65515989a709e7987da49
ToxicFrazzles <frazzles740@gmail.com>
2021-06-04 13:43:14 +0100

    Think the updater is ready?
    Currently it only updates when a push has occurred whilst the updater is running

---
+++ update_watcher.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import asyncio
+import concurrent.futures
+from pathlib import Path
+import os
+import websockets.client
+import websockets.exceptions
+
+this_dir = Path(__file__).parent.resolve()
+
+
+def update():
+    os.system(f"cd {this_dir}")
+    os.system(f"git pull")
+
+
+class Updater:
+    def __init__(self):
+        self.connection = None
+        self.uri = "wss://blokegaming.com/whs/sockets/h85hX5pLFlfBOKfquSS5cujMOFt1J8UWIfY0HYOS9JkIuENs0rUoPKUUDkotvVbb"
+        self.receiveTask = None
+        self.heartbeatTask = None
+        self.quitting = False
+
+    async def connect(self):
+        while not self.quitting:
+            self.connection = await websockets.client.connect(self.uri)
+            self.receiveTask = asyncio.create_task(self.receiveMessage())
+            self.heartbeatTask = asyncio.create_task(self.heartbeat())
+            await self.connection.wait_closed()
+
+    async def receiveMessage(self):
+        loop = asyncio.get_event_loop()
+        executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+        async for message in self.connection:
+            print("A push event has occurred on GitHub")
+            await loop.run_in_executor(executor, update)
+            self.quitting = True
+            await self.connection.close()
+
+    async def heartbeat(self):
+        while True:
+            try:
+                await self.connection.send('ping')
+                await asyncio.sleep(5)
+            except websockets.exceptions.ConnectionClosed:
+                break
+
+
+# async def main():
+#     executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+#     loop = asyncio.get_event_loop()
+#     async with websockets.connect(
+#             uri
+#     ) as websocket:
+#         async for message in websocket:
+#             print("A push event has occurred on GitHub")
+#             await loop.run_in_executor(executor, update)
+
+if __name__ == "__main__":
+    updater = Updater()
+    asyncio.get_event_loop().run_until_complete(updater.connect())
