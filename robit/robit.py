8a8edac7f728ea87f579b4e05da08ae71bab6bbe
ToxicFrazzles <frazzles740@gmail.com>
2021-06-05 14:48:56 +0100

    Just a quick test of the arduino
    Should move forward slowly for 1 second upon booting up.

--- robit/robit.py
+++ robit/robit.py
@@ -1,5 +1,6 @@
 from .websocket import Websocket
 from .updater import Updater
+from .arduino import ArduinoConsumer
 from .exceptions import ExitNicelyException
 import asyncio
 
@@ -9,6 +10,7 @@ class Robit:
         self.ws = Websocket(ws_url, key)
         self.updater = Updater()
         self.ws.message_handler = self.consumer_handler
+        self.arduino = ArduinoConsumer()
 
     async def consumer_handler(self, message):
         if message["type"] == "update":
@@ -18,6 +20,9 @@ class Robit:
                 await self.ws.disconnect()
 
     async def _run(self):
+        await self.arduino.handle({"command": "motors", "left": 10, "right": 10})
+        await asyncio.sleep(1)
+        await self.arduino.handle({"command": "motors", "left": 0, "right": 0})
         await self.ws.connect()
         # asyncio.create_task(self.ws.connect())
 
