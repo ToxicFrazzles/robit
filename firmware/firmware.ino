5ce673664764a64bb34fb644a04fd3928dc75696
ToxicFrazzles <frazzles740@gmail.com>
2021-06-05 10:25:05 +0100

    Test sketch
    Should blink the LED once a second.
    If the auto update works then the inside of the robot should flash.
    Also updated the example service file.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,7 +1,11 @@
-void setup(){
 
+void setup(){
+	pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop(){
-
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(500);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(500);
 }
