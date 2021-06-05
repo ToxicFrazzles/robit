f5b8ccbf654093343f4bc44b6f05d1d96eb4c146
ToxicFrazzles <frazzles740@gmail.com>
2021-06-05 17:24:03 +0100

    Think I figured the problem...

--- robit/arduino.py
+++ robit/arduino.py
@@ -23,7 +23,7 @@ def read_message(conn):
 class ArduinoConsumer(BaseConsumer):
     def __init__(self):
         super().__init__()
-        self.serial = serial.Serial("/dev/ttyUSB0", baudrate=9600)
+        self.serial = serial.Serial("/dev/ttyUSB0", 9600)
 
     async def receive_messages(self):
         message = await read_message(self.serial)
