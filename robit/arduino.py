87b1d05ab54b31d2e3412d100ab60a1ca7c25806
ToxicFrazzles <frazzles740@gmail.com>
2021-06-05 15:10:13 +0100

    Trying to ensure arduino communication is working

--- robit/arduino.py
+++ robit/arduino.py
@@ -6,7 +6,7 @@ from .utils import sync_to_async
 
 @sync_to_async
 def send_message(conn, data):
-    conn.write(data)
+    return conn.write(data)
 
 
 class ArduinoConsumer(BaseConsumer):
@@ -20,4 +20,7 @@ class ArduinoConsumer(BaseConsumer):
             left = message["left"]
             right = message["right"]
             data = f"m{left},{right};".encode('utf-8')
-            await send_message(self.serial, data)
+            print(data)
+            byte_count = await send_message(self.serial, data)
+            if byte_count != len(data):
+                print(f"Only sent {byte_count} out of {len(data)} bytes")
